0ad9284a60f99960f84a61f42f62311702f94991
Yono1112 <yuma.ohno.sss@gmail.com>
2024-04-08 13:24:06 +0900

    add iter.py

---
+++ python_tutorial/iter.py
@@ -0,0 +1,12 @@
+for element in [1, 2, 3]:
+    print(element)
+for element in (1, 2, 3):
+    print(element)
+for key in {"one": 1, "two": 2}:
+    print(key)
+for value in {"one": 1, "two": 2}.values():
+    print(value)
+for key, value in {"one": 1, "two": 2}.items():
+    print(key, value)
+for char in "123":
+    print(char)
--- python_tutorial/person.py
+++ python_tutorial/person.py
@@ -1,7 +1,17 @@
 class Person:
+    """This is documentation string"""
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def display_info(self):
         print(f"Name: {self.name}, Age: {self.age}")
+        # selfで他のメソッドを呼び出せる
+        # self.display_name()
+        # self.display_age()
+
+    def display_name(self):
+        print(f"Name: {self.name}")
+
+    def display_age(self):
+        print(f"Age: {self.age}")
