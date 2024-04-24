bc928682a9703d772e7787ca5ec755e29fb2d7bb
Yono1112 <yuma.ohno.sss@gmail.com>
2024-04-24 19:51:32 +0900

    create my_page function

--- Django_tutorial_poll_site/user_auth/views.py
+++ Django_tutorial_poll_site/user_auth/views.py
@@ -1,5 +1,8 @@
 from django.views import generic
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.models import User
+
 
 from .forms import LoginForm
 
@@ -15,3 +18,18 @@ class Login(LoginView):
 
 class Logout(LogoutView):
     template_name = 'user_auth/logout_done.html'
+
+
+class OnlyYouMixin(UserPassesTestMixin):
+    raise_exception = True
+
+    def test_func(self):
+        # 今ログインしてるユーザーのpkと、そのマイページのpkが同じなら許可
+        user = self.request.user
+        return user.pk == self.kwargs['pk']
+
+
+class MyPage(OnlyYouMixin, generic.DetailView):
+    model = User
+    template_name = 'user_auth/my_page.html'
+    # モデル名小文字(user)でモデルインスタンスがテンプレートファイルに渡される
