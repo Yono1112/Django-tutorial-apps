61197ac997f6ed89e57768c9409fcc55b56402a6
Yono1112 <yuma.ohno.sss@gmail.com>
2024-05-06 12:51:43 +0900

    set autofocus when creating new Question

--- Django_tutorial_poll_site/polls/forms.py
+++ Django_tutorial_poll_site/polls/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, inlineformset_factory
+from django.forms import ModelForm, inlineformset_factory, TextInput
 from .models import Question, Choice
 
 
@@ -6,6 +6,9 @@ class NewQuestionForm(ModelForm):
     class Meta:
         model = Question
         fields = ["question_text"]
+        widgets = {
+            'question_text': TextInput(attrs={'autofocus': 'autofocus'}),
+        }
 
 
 ChoiceFormSet = inlineformset_factory(Question, Choice, fields=('choice_text',), extra=3, can_delete=False)
