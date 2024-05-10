33dde9964cc914e3df9e19300da5461ecb2c7653
Yono1112 <yuma.ohno.sss@gmail.com>
2024-05-10 15:46:18 +0900

    add script.js

---
+++ Django_tutorial_poll_site/static/js/script.js
@@ -0,0 +1,11 @@
+// const btn = document.querySelector(".add-btn")
+// const element = document.querySelector(".hello")
+
+// btn.addEventListener("click", () => {
+
+// 	const para = document.createElement("p");
+// 	para.textContent = "We hope you enjoyed the ride.";
+
+// 	element.appendChild(para);
+
+// });
